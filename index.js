8ba6f95c4fa344abe250ba29f5ff4ed6f02fc0df
7029732+shivlim@users.noreply.github.com <7029732+shivlim@users.noreply.github.com>
2022-03-18 10:17:25 +0000

    updated

--- index.js
+++ index.js
@@ -1,14 +1,122 @@
 const WebSocket = require('ws');
 const axios = require('axios');
 const Slimbot = require('slimbot');
-const https = require("https");
 require('dotenv').config()
-const ws = new WebSocket('ws://127.0.0.1:26657/websocket');
+const TELEGRAMBOTTOKEN = process.env.TELEGRAM_BOT_TOKEN
+const TELEGRAMCHATID = process.env.TELEGRAM_CHAT_ID
+const WSURL = process.env.WS_URL
+const TXURI = process.env.TX_URI
+const RPCSYNCCHECKRUNINTERVALINMINS = process.env.RPC_SYNC_CHECK_RUN_INTERVAL_IN_MINS
+const DEADMANSSWITCHRUNINTERVALINMINS = process.env.DEAD_MANS_SWITCH_RUN_INTERVAL_IN_MINS
+const ETHRPCENDPOINT = process.env.ETH_RPC_ENDPOINT
+const MOONBEAMRPCENDPOINT = process.env.MOONBEAM_RPC_ENDPOINT
+const FANTOMRPCENDPOINT = process.env.FANTOM_RPC_ENDPOINT
+const AVAXRPCENDPOINT = process.env.AVAX_RPC_ENDPOINT
+const POLYGONRPCENDPOINT = process.env.POLYGON_RPC_ENDPOINT
+const ETH_RPC_ENDPOINT_REQUEST = {"id": 1, "jsonrpc": "2.0", "method": "eth_syncing", "params": []}
+const MOONBEAM_RPC_ENDPOINT_REQUEST = {"id": 1, "jsonrpc": "2.0", "method": "eth_syncing", "params": []}
+const FANTOM_RPC_ENDPOINT_REQUEST = {"id": 1, "jsonrpc": "2.0", "method": "eth_syncing", "params": []}
+const AVAX_RPC_ENDPOINT_REQUEST = {"jsonrpc": "2.0","method": "info.isBootstrapped","params":{"chain":"C"},"id":1}
+const POLYGON_RPC_ENDPOINT_REQUEST = {"id": 1, "jsonrpc": "2.0", "method": "eth_syncing", "params": []}
+
+
+
+const ws = new WebSocket(WSURL);
+const slimbot = new Slimbot(TELEGRAMBOTTOKEN);
 
 ws.on('open', function open() {
     console.log('Open now')
     ws.send('{ "jsonrpc": "2.0","method": "subscribe","id": 0,"params": {"query": "tm.event=\'Tx\' AND depositConfirmation.action=\'vote\' AND transfer.recipient=\'axelar1p8vpmajj4lym6x7r8wn0zjpdthah6ghx80lphe\' AND depositConfirmation.value=\'true\'"}}');
 });
+
+async function deadmansswitch(){
+    const alertmsg = `<b>ALIVE</b>`;
+    slimbot.sendMessage(TELEGRAMCHATID, alertmsg,{parse_mode: 'HTML'});
+}
+
+async function checksyncstatus(){
+    let rpcrequestmap = [
+        {
+            "rpcendpoint":ETHRPCENDPOINT,
+            "rpcendpointrequest":ETH_RPC_ENDPOINT_REQUEST
+        },
+        {
+            "rpcendpoint":MOONBEAMRPCENDPOINT,
+            "rpcendpointrequest":MOONBEAM_RPC_ENDPOINT_REQUEST
+        },
+        {
+            "rpcendpoint":FANTOMRPCENDPOINT,
+            "rpcendpointrequest":FANTOM_RPC_ENDPOINT_REQUEST
+        },
+        {
+            "rpcendpoint":AVAXRPCENDPOINT,
+            "rpcendpointrequest":AVAX_RPC_ENDPOINT_REQUEST
+        },
+        {
+            "rpcendpoint":POLYGONRPCENDPOINT,
+            "rpcendpointrequest":POLYGON_RPC_ENDPOINT_REQUEST
+        }
+
+    ]
+
+    Promise.all(rpcrequestmap.map((endpoint) => axios.post(endpoint.rpcendpoint,endpoint.rpcendpointrequest)))
+        .then(([{data: ethdata}, {data: moonbeamdata}, {data: fantomdata}, {data: avaxdata},{data: polygondata}] )=> {
+          console.log({ ethdata, moonbeamdata, fantomdata, avaxdata,polygondata });
+          let synced='✅'
+          let outofsync='❌'
+            let ethstatus=outofsync;
+            let moonbeamstatus=outofsync;
+            let fantomstatus=outofsync;
+            let avaxstatus=outofsync;
+            let polygonstatus=outofsync;
+            if(!ethdata.result){
+                ethstatus = synced;
+            }
+            if(!moonbeamdata.result){
+                moonbeamstatus = synced;
+            }
+            if(!fantomdata.result){
+                fantomstatus = synced;
+            }
+            if(avaxdata.result.isBootstrapped){
+                avaxstatus = synced;
+            }
+            if(!polygonstatus.result){
+                polygonstatus = synced;
+            }
+            const alertmsg = `<b>ETHSyncStatus</b>: ${ethstatus}
+                               <b>MoonbeamSyncStatus</b>: ${moonbeamstatus}
+                               <b>FantomStatus</b>: ${fantomstatus}
+                               <b>AvaxStatus</b>: ${avaxstatus}
+                               <b>PolygonStatus</b>: ${polygonstatus}`;
+            slimbot.sendMessage(TELEGRAMCHATID, alertmsg,{parse_mode: 'HTML'});
+
+
+    });
+
+}
+
+function alertfornovotes(txurl){
+    axios.get(txurl)
+        .then(function (response) {
+            // handle success
+            const responsebody = response.data;
+            console.log(responsebody);
+            const chain = responsebody['tx']['body']['messages'][0]['inner_message']['chain']
+            console.log("chain is %s",chain);
+            const alertmsg = `<b>ALERT</b>: Voted <b>NO</b> on Chain <b>${chain}</b>`;
+            slimbot.sendMessage(TELEGRAMCHATID, alertmsg,{parse_mode: 'HTML'});
+        })
+        .catch(function (error) {
+            console.log(error);
+            const alertmsg = `<b>ALERT</b>: Voted <b>NO</b>`;
+            slimbot.sendMessage(TELEGRAMCHATID, alertmsg,{parse_mode: 'HTML'});
+        })
+        .then(function () {
+            // always executed
+        });
+
+}
 
 ws.on('message', function message(data) {
     console.log('received: %s', data);
@@ -16,27 +124,15 @@ ws.on('message', function message(data) {
     const response = JSON.parse(data)
     if(response.hasOwnProperty('result') &&  response['result'].hasOwnProperty('events')){
         const txhash = response['result']['events']['tx.hash']
-        const txurl = 'http://localhost:1317/cosmos/tx/v1beta1/txs/' + txhash;
+        const txurl = TXURI + txhash;
         console.log('txurl is' + txurl)
-        axios.get(txurl)
-            .then(function (response) {
-                // handle success
-                const responsebody = response.data;
-                console.log(responsebody);
-                console.log(responsebody['tx']['body']['messages'][0]['inner_message']['chain']);
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-            .then(function () {
-                // always executed
-            });
+        setTimeout(() => { alertfornovotes(txurl) }, 20000);
     }else{
         console.log('no own property result')
     }
 
 });
 
-
-
+setInterval(checksyncstatus, RPCSYNCCHECKRUNINTERVALINMINS * 60  * 1000);
+setInterval(deadmansswitch, DEADMANSSWITCHRUNINTERVALINMINS * 60  * 1000);
+deadmansswitch();
