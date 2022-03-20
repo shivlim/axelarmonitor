90a0726a4ec935ee63f655de5fc1a6c592bcb99b
7029732+shivlim@users.noreply.github.com <7029732+shivlim@users.noreply.github.com>
2022-03-20 21:28:19 +0000

    Error resubscription

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ function alertfornovotes(txurl){
 function subscribetowss() {
     ws.on('open', function open() {
         console.log('Open now')
-        const subscriberequest = `{ "jsonrpc": "2.0","method": "subscribe","id": 0,"params": {"query": "tm.event=\'Tx\' AND depositConfirmation.action=\'vote\' AND transfer.recipient=\'${AXELARBROADCASTERADDRESS}\' AND depositConfirmation.value=\'true\'"}}`
+        const subscriberequest = `{ "jsonrpc": "2.0","method": "subscribe","id": 0,"params": {"query": "tm.event=\'Tx\' AND depositConfirmation.action=\'vote\' AND transfer.recipient=\'${AXELARBROADCASTERADDRESS}\' AND depositConfirmation.value=\'false\'"}}`
         ws.send(subscriberequest);
     });
 }
