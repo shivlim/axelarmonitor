cb4addf8f5272a810058dabab09b63024652b482
7029732+shivlim@users.noreply.github.com <7029732+shivlim@users.noreply.github.com>
2022-10-29 19:44:53 +0100

    Added screeenshot

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Slimbot = require('slimbot');
 require('dotenv').config()
 const TELEGRAMBOTTOKEN = process.env.TELEGRAM_BOT_TOKEN
 const TELEGRAMCHATID = process.env.TELEGRAM_CHAT_ID
+const MSG_PREFIX = process.env.MSG_PREFIX
 const RPCSYNCCHECKRUNINTERVALINMINS = process.env.RPC_SYNC_CHECK_RUN_INTERVAL_IN_MINS
 const DEADMANSSWITCHRUNINTERVALINMINS = process.env.DEAD_MANS_SWITCH_RUN_INTERVAL_IN_MINS
 const NOVOTECHECKINTERVALINMINS = process.env.NO_VOTE_CHECK_INTERVAL_IN_MINS
@@ -30,17 +31,6 @@ const TCP_CONNECT_TIMEOUT_IN_MS = 10000
 
 
 const slimbot = new Slimbot(TELEGRAMBOTTOKEN);
-//const now = new Date()
-/*const utcMilllisecondsSinceEpoch = now.getTime() + (now.getTimezoneOffset() * 60 * 1000)
-const utcSecondsSinceEpoch = Math.round(utcMilllisecondsSinceEpoch / 1000)
-console.log('utcSecondsSinceEpoch' + utcSecondsSinceEpoch)*/
-//console.log('utcSecondsSinceEpochutc ' + new Date(new Date().toUTCString()).valueOf())
-
-//let utcdate = new Date(new Date().toUTCString())
-//utcdate.setMinutes(utcdate.getMinutes() - 10)
-//console.log('utcSecondsSinceEpochutcminus ' + utcdate.valueOf())
-
-
 async function checksyncstatus(...deadmanswitchflag){
     let rpcrequestmap = [
         {
@@ -83,7 +73,7 @@ async function checksyncstatus(...deadmanswitchflag){
         source.cancel();
     }, TCP_CONNECT_TIMEOUT_IN_MS);
 
-    Promise.allSettled(rpcrequestmap.map((endpoint) => axios.post(endpoint.rpcendpoint,endpoint.rpcendpointrequest,{cancelToken: source.token})))
+    Promise.allSettled(rpcrequestmap.map((endpoint) => axios.post(endpoint.rpcendpoint,endpoint.rpcendpointrequest)))
         .then((results) => {
             let ethresult = results[0]
             let moonbeamresult = results[1]
@@ -155,41 +145,14 @@ function getlogo(ethstatus) {
         return '✅';
     else return '❌';
 }
-//curl 'https://testnet.api.axelarscan.io/heartbeats' \
-//   -H 'authority: testnet.api.axelarscan.io' \
-//   -H 'accept: */*' \
-//   -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8' \
-//   -H 'cache-control: no-cache' \
-//   -H 'content-type: text/plain;charset=UTF-8' \
-//   -H 'origin: https://docs.axelarscan.io' \
-//   -H 'pragma: no-cache' \
-//   -H 'referer: https://docs.axelarscan.io/' \
-//   -H 'sec-ch-ua: "Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"' \
-//   -H 'sec-ch-ua-mobile: ?0' \
-//   -H 'sec-ch-ua-platform: "macOS"' \
-//   -H 'sec-fetch-dest: empty' \
-//   -H 'sec-fetch-mode: cors' \
-//   -H 'sec-fetch-site: same-site' \
-//   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36' \
-//   --data-raw '{"sender":"axelar1rym08uqf7z3ccjr9q2x8zu8acrxq03n346snxt","size":5}' \
-//   --compressed
+
 async function checkheartbeat(){
     let requestbody = {}
-    /**
-     * {
 
-	"voter": "axelar1d2pes5he2u756gscwewfammkhurelsghmjzcex",
-     "vote":"no",
-      "fromTime":"1666299208",
-	"size": 10
-}
-     */
     let utcdate = new Date(new Date().toUTCString())
-    utcdate.setMinutes(utcdate.getMinutes() - NOVOTECHECKINTERVALINMINS)
-    // let curutctimeinsec = new Date(new Date().toUTCString()).valueOf()
-    //date.setMinutes(date.getMinutes() - numOfMinutes);
+    let curutcepoch = Math.round(utcdate.valueOf())
     requestbody['sender'] = AXELAR_BROADCASTER_ADDRESS
-    requestbody['size'] = 5
+    requestbody['size'] = 1
 
     const headers = {
         'Content-Type': 'application/json'
@@ -197,58 +160,28 @@ async function checkheartbeat(){
     const json = JSON.stringify(requestbody);
     const res = await axios.post(AXELARSCAN_HEARTBEAT_API_URL, json,{headers: headers});
     console.log(res.data.data)
-    res.data.data.forEach(poll=>{
-        let totalparticipants = poll['participants'].length
-        let pollid = poll['id']
-        let chain = poll['sender_chain']
-        let nooffalsevotesinpoll = 0;
-        Object.keys(poll).forEach(key=>{
-            //console.log(key)
-            if(key.startsWith('axelar1')){
-                let vote = poll[key]['vote']
-                if(vote === false){
-                    nooffalsevotesinpoll++;
-                }
-            }
-        })
-        console.log(totalparticipants)
-        console.log(nooffalsevotesinpoll)
-        console.log(' percentage of false votes ' + (nooffalsevotesinpoll/totalparticipants))
-        if((nooffalsevotesinpoll/totalparticipants)<=0.5){
-            //raise alarm
-            slimbot.sendMessage(TELEGRAMCHATID, 'voted ❌ for ' + chain + ' in poll id '+ pollid,);
-            console.log('voted false for ' + chain + ' in poll id '+ pollid)
-        }
-
-    })
-
+    let lastheartbeattimestamp = res.data.data[0]['timestamp']
+    let deltainmins = (curutcepoch - lastheartbeattimestamp)/60000
+    console.log('heartbeat delta in mins is ' + deltainmins)
+    if(deltainmins >= 10){
+        slimbot.sendMessage(TELEGRAMCHATID, MSG_PREFIX + ' : stale heartbeat on axelar');
+        console.log('stale heartbeat')
+    }
 }
 
 async function checknovotes(){
     let requestbody = {}
-    /**
-     * {
-
-	"voter": "axelar1d2pes5he2u756gscwewfammkhurelsghmjzcex",
-     "vote":"no",
-      "fromTime":"1666299208",
-	"size": 10
-}
-     */
     let utcdate = new Date(new Date().toUTCString())
     utcdate.setMinutes(utcdate.getMinutes() - NOVOTECHECKINTERVALINMINS)
-   // let curutctimeinsec = new Date(new Date().toUTCString()).valueOf()
-    //date.setMinutes(date.getMinutes() - numOfMinutes);
     requestbody['voter'] = AXELAR_BROADCASTER_ADDRESS
     requestbody['vote'] = 'no'
     requestbody['fromTime'] =  String(Math.round(utcdate.valueOf() / 1000))
-    //requestbody['fromTime'] =  '1666299208'
     requestbody['size'] = 10
     const headers = {
         'Content-Type': 'application/json'
     }
     const json = JSON.stringify(requestbody);
-    const res = await axios.post(AXELARSCAN_API_URL, json,{headers: headers});
+    const res = await axios.post(AXELARSCAN_EVMPOLL_API_URL, json,{headers: headers});
     console.log(res.data.data)
     res.data.data.forEach(poll=>{
         let totalparticipants = poll['participants'].length
@@ -269,7 +202,7 @@ async function checknovotes(){
         console.log(' percentage of false votes ' + (nooffalsevotesinpoll/totalparticipants))
         if((nooffalsevotesinpoll/totalparticipants)<=0.5){
             //raise alarm
-            slimbot.sendMessage(TELEGRAMCHATID, 'voted ❌ for ' + chain + ' in poll id '+ pollid,);
+            slimbot.sendMessage(TELEGRAMCHATID, MSG_PREFIX + ' voted ❌ for ' + chain + ' in poll id '+ pollid,);
             console.log('voted false for ' + chain + ' in poll id '+ pollid)
         }
 
@@ -277,8 +210,8 @@ async function checknovotes(){
 
 }
 
-//setInterval(checksyncstatus, RPCSYNCCHECKRUNINTERVALINMINS * 60  * 1000,false);
-//setInterval(checksyncstatus, DEADMANSSWITCHRUNINTERVALINMINS * 60  * 1000,true);
-//checksyncstatus(true);
-//checknovotes();
-checkheartbeat();
+setInterval(checksyncstatus, RPCSYNCCHECKRUNINTERVALINMINS * 60  * 1000,false);
+setInterval(checksyncstatus, DEADMANSSWITCHRUNINTERVALINMINS * 60  * 1000,true);
+checksyncstatus(true);
+setInterval(checknovotes, 3 * 60  * 1000);
+setInterval(checkheartbeat, 3 * 60  * 1000);
